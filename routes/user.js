40a9921d864500be0bf69860072262194d270fc2
Abubakar Sambo <sadiqasg@gmail.com>
2020-01-25 22:10:53 +0100

    removed get single user

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,5 @@ const userCtrl = require('../controllers/user');
 router.post('/signup', userCtrl.signUp);
 router.post('/login', userCtrl.login);
 router.get('/users', userCtrl.getUsers);
-router.get('/users/:id', userCtrl.getSingleUser);
 
 module.exports = router;
